955a0c1b982c58c6e65b4917efa7513eb8df855d
adanmc01 <dev.adanmc01@gmail.com>
2021-07-21 19:54:17 -0500

    fix SF connect

--- src/routes/salesForceIn.js
+++ src/routes/salesForceIn.js
@@ -5,22 +5,41 @@ const router = Router();
 router.post('/salesForceIn', async (req, res) => {
     try {
        //define the ContactId to find in salesForce
+        let conn
+        let contactEmail
         const payload = req.body.new
         console.log(payload[0])
         const contactId = payload[0].ContactId
         //Connect to SalesForce (login)
+        conn = await new jsforce.Connection({
+            // you can change loginUrl to connect to sandbox or prerelease env.
+            // loginUrl : 'https://test.salesforce.com'
+        });
+        await conn.login(process.env.SF_USERNAME, process.env.SF_PASSWORD + process.env.SF_TOKEN, function (err, userInfo) {
+            if (err) {
+                return console.error(err);
+            }
+            // Now you can get the access token and instance URL information.
+            // Save them to establish connection next time.
+            console.log("Connect salesForce Done!")
+            console.log(conn.accessToken);
+            console.log(conn.instanceUrl);
+            // logged in user property
+            console.log("User ID: " + userInfo.id);
+            console.log("Org ID: " + userInfo.organizationId);
+            // ...
+        });
         // Find contact by Id on SalesForce
-
-        // let connect = new jsforce.sobject("Contact")
-        //     .find({Id: contactId}) // "fields" argument is omitted
-        //     .execute(function (err, records) {
-        //         if (err) {
-        //             return console.error(err);
-        //         }
-        //         contactEmail = records
-        //         console.log(records);
-        //     });
-        // await console.log(contactEmail)
+        await conn.sobject("Contact")
+            .find({Id: contactId}) // "fields" argument is omitted
+            .execute(function (err, records) {
+                if (err) {
+                    return console.error(err);
+                }
+                contactEmail = records
+                console.log(records);
+            });
+        await console.log(contactEmail)
         // Conectar con RD
 
         // usar contact.Email para actualizar contacto por email en RD
@@ -31,9 +50,6 @@ router.post('/salesForceIn', async (req, res) => {
        //  }).catch((error) => {
        //      console.error(error)
        //  })
-
-
-
 //
         res.json({
             success: true,
